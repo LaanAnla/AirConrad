c78041d76c95197202a9dc11509899d957e1c7d7
LaanAnla <95160853+LaanAnla@users.noreply.github.com>
2023-07-28 15:53:57 +0200

    scrollsmoother mobile 3

--- app/index.js
+++ app/index.js
@@ -17,8 +17,8 @@ class App {
     this.smoother = ScrollSmoother.create({
       smooth: 2,               // how long (in seconds) it takes to "catch up" to the native scroll position
       effects: true,           // looks for data-speed and data-lag attributes on elements
-      //smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
-      //normalizeScroll: true
+      smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+      normalizeScroll: true
     })
 
     this.canvas = document.querySelector('canvas#webgl')
